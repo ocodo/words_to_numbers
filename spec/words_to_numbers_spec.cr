2a89d25fbc476367ee4102cce006b09aea443342
jasonm23 <jasonm23@gmail.com>
2020-08-29 11:37:37 +0800

    Improve specs, add 'a', 'pair' and 'couple' parsing

--- spec/words_to_numbers_spec.cr
+++ spec/words_to_numbers_spec.cr
@@ -2,25 +2,35 @@ require "./spec_helper"
 
 describe "Words to numbers" do
 
-  examples = [
-    {"none", 0},
-    {"one", 1},
-    {"twenty-five", 25},
-    {"minus one hundred and seventeen", -117},
-    {"one hundred and fifty-six", 156},
-    {"minus two thousand two", -2002},
-    {"nine thousand, seven hundred, one", 9701},
-    {"minus six hundred and twenty six thousand, eight hundred and fourteen", -626814},
-    {"four million, seven hundred thousand, three hundred and eighty-six", 4700386},
-    {"fifty-one billion, two hundred and fifty-two million, seventeen thousand, one hundred eighty-four", 51252017184},
-    {"two hundred and one billion, twenty-one million, two thousand and one", 201021002001}
-  ]
+  {% for example in {
+     {"none", 0},
+     {"one", 1},
+     {"a couple hundred", 200},
+     {"couple thousand", 2000},
+     {"a couple of million", 2000000},
+     {"a couple", 2},
+     {"a pair", 2},
+     {"twenty-five", 25},
+     {"minus one hundred and seventeen", -117},
+     {"one hundred and fifty-six", 156},
+     {"minus two thousand two", -2002},
+     {"nine thousand, seven hundred, one", 9701},
+     {"minus six hundred and twenty six thousand, eight hundred and fourteen", -626814},
+     {"four million, seven hundred thousand, three hundred and eighty-six", 4700386},
+     {"fifty-one billion, two hundred and fifty-two million, seventeen thousand, one hundred eighty-four", 51252017184},
+     {"two hundred and one billion, twenty-one million, two thousand and one", 201021002001},
+     {"a thousand", 1000},
+     {"a billion", 1000000000},
 
-  it "converts words to a number" do
-    examples.each do |example|
-      str = example[0]
-      num = example[1]
+   }
+    %}
+
+    it "\"#{{{example[0]}}}\" = #{{{example[1]}}}" do
+      str = {{example[0]}}
+      num = {{example[1]}}
       words_to_number(str).should eq num
     end
-  end
+
+  {% end %}
+
 end
--- src/words_to_numbers.cr
+++ src/words_to_numbers.cr
@@ -5,9 +5,9 @@ VERSION = "0.1.0"
 SMALL = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
   "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"}
 
-TENS = {"_", "_", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"}
+TENS = {" ", " ", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"}
 
-BIG = {"_", "thousand", "million", "billion", "trillion", "quadrillion",
+BIG = {" ", "thousand", "million", "billion", "trillion", "quadrillion",
        "quintillion", "sextillion", "septillion", "octillion", "nonillion", "decillion"}
 
 def words_to_number(words_in : String) : BigInt
@@ -26,25 +26,28 @@ def words_to_number(words_in : String) : BigInt
 
   words.each do |word|
     case word
+    when "hundred"
+      small *= 100
     when "none"
       total = 0.to_big_i
+    when "a"
+      small += 1.to_big_i
+    when .matches? %r(couple|pair)
+      small = 2.to_big_i
     when .in? SMALL
       index = SMALL.index(word)
       small += index unless index.nil?
     when .in? TENS
       index = TENS.index(word)
       small += index * 10 unless index.nil?
-    when "hundred"
-      small *= 100
-    when "thousand"
-      total += small * 1000
-      small = 0.to_big_i
     when .in? BIG
       index = BIG.index(word)
-      total += small * 1000 ** index unless index.nil?
+      total += small * (1000 ** index) unless index.nil?
       small = 0.to_big_i
+    when "of"
+      nil
     else
-      raise ArgumentError.new("Cannot understand #{word} in \"#{words_in}\"")
+      raise ArgumentError.new("Cannot understand \"#{word}\" in \"#{words_in}\"")
     end
   end
 
